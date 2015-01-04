4e117f58221cb33d0422fc5c3de4ca6edbdf506b
jfms7s <jfms7s@gmail.com>
2015-01-04 23:34:10 +0000

    Changed the onto to hold series

--- RDF_Populate/main.py
+++ RDF_Populate/main.py
@@ -31,6 +31,12 @@ while thisYear <= lastYear:
 	MovieJSON = json.load(FileData)
 	FileData.close()
 
+	filename = 'series_' + str(thisYear) + '.json'
+	#load all json files to store in RDF
+	FileData = open(filename)
+	SeriesJSON = json.load(FileData)
+	FileData.close()
+
 	filename = 'persons_' + str(thisYear) + '.json'
 	FileData = open(filename)
 	PersonJSON = json.load(FileData)
@@ -56,10 +62,7 @@ while thisYear <= lastYear:
 
 		PersonNode = URIRef(LocalNamespace[person['id']]);
 
-		if(person['actor']):
-			g.add((PersonNode,RDF.type,ns.Actor))
-		else:
-			g.add((PersonNode,RDF.type,ns.Director))
+		g.add((PersonNode,RDF.type,ns.Person))
 
 		for attr in person:
 			if(attr=='id'):
@@ -71,15 +74,17 @@ while thisYear <= lastYear:
 			if(attr=='miniBio'):
 				g.add((PersonNode,ns.hasPersonMiniBio,Literal(person[attr])))
 			if(attr=='jobCategories'):
-				pprint(person[attr])
+				for prof in person[attr]:
+					g.add((PersonNode,ns.hasProfession,ns[prof.replace(" ","_")]))
+					pprint(ns[prof.replace(" ","_")])
 			if(attr=='birthPlace'):
 				g.add((PersonNode,ns.hasPersonBirthPlace,Literal(person[attr])))
 			if(attr=='birthDate'):
 				g.add((PersonNode,ns.hasPersonBirth,Literal(person[attr], datatype=XSD.date)))
-			if(attr=='actor'):
-				pprint(person[attr])
-			if(attr=='director'):
-				pprint(person[attr])
+			#if(attr=='actor'):
+			#	pprint(person[attr])
+			#if(attr=='director'):
+			#	pprint(person[attr])
 
 	#
 	# STORE STUDIOS IN TREE
@@ -109,7 +114,7 @@ while thisYear <= lastYear:
 
 	for movie in MovieJSON:
 		#see if movie is already added
-		search = g.value(predicate=ns.hasMovieId,object=Literal(movie['id']))
+		search = g.value(predicate=ns.hasMediaId,object=Literal(movie['id']))
 		if(search!=None):
 			pprint("Error:Theres already an Instance for the Movie id "+movie['id'])
 			continue;
@@ -119,7 +124,7 @@ while thisYear <= lastYear:
 
 		for attr in movie:
 			if(attr=='id'):
-				g.add((MovieNode,ns.hasMovieId,Literal(movie[attr])))
+				g.add((MovieNode,ns.hasMediaId,Literal(movie[attr])))
 			if(attr=='directors'):
 				for director in movie[attr]['director']:
 					search = g.value(predicate=ns.hasPersonId, object=Literal(director))
@@ -135,17 +140,16 @@ while thisYear <= lastYear:
 						continue;
 					g.add((MovieNode,ns.hasStudio,search))
 			if(attr=='launchDate'):
-				g.add((MovieNode,ns.hasMovieLaunchDate,Literal(movie[attr], datatype=XSD.date)))
+				g.add((MovieNode,ns.hasMediaLaunchDate,Literal(movie[attr], datatype=XSD.date)))
 			if(attr=='genres'):
-				pprint(movie[attr])
 				for genre in movie[attr]['genre']:
-					g.add((MovieNode,ns.hasGenres,ns[genre]))
+					g.add((MovieNode,ns.hasGenre,ns[genre]))
 			if(attr=='description'):
-				g.add((MovieNode,ns.hasMovieDescription,Literal(movie[attr])))
+				g.add((MovieNode,ns.hasMediaDescription,Literal(movie[attr])))
 			if(attr=='score'):
-				g.add((MovieNode,ns.hasMovieClassification,Literal(movie[attr], datatype=XSD.float)))
+				g.add((MovieNode,ns.hasMediaClassification,Literal(movie[attr], datatype=XSD.float)))
 			if(attr=='duration'):
-				g.add((MovieNode,ns.hasMovieDuration,Literal(movie[attr], datatype=XSD.integer)))
+				g.add((MovieNode,ns.hasMediaDuration,Literal(movie[attr], datatype=XSD.integer)))
 			if(attr=='stars'):
 				for star in movie[attr]['star']:
 					search = g.value(predicate=ns.hasPersonId,object=Literal(star))
@@ -154,9 +158,64 @@ while thisYear <= lastYear:
 						continue;
 					g.add((MovieNode,ns.hasActor,search))
 			if(attr=='name'):
-				g.add((MovieNode,ns.hasMovieName,Literal(movie[attr])))
+				g.add((MovieNode,ns.hasMediaName,Literal(movie[attr])))
 			if(attr=='image'):
-				g.add((MovieNode,ns.hasMoviePoster,Literal(movie[attr])))
+				g.add((MovieNode,ns.hasMediaPoster,Literal(movie[attr])))
+
+	#
+	# STORE SERIES IN TREE
+	#########################
+
+	LocalNamespace=Namespace('http://www.movierecomendation.pt/Serie/')
+
+	for serie in SeriesJSON:
+		#see if movie is already added
+		search = g.value(predicate=ns.hasMediaId,object=Literal(serie['id']))
+		if(search!=None):
+			pprint("Error:Theres already an Instance for the Serie id "+serie['id'])
+			continue;
+
+		SerieNode = URIRef(LocalNamespace[serie['id']]);
+		g.add((SerieNode,RDF.type,ns.Serie))
+
+		for attr in serie:
+			if(attr=='id'):
+				g.add((SerieNode,ns.hasMediaId,Literal(serie[attr])))
+			if(attr=='studios'):
+				for studio in serie[attr]['studio']:
+					search = g.value(predicate=ns.hasStudioId, object=Literal(studio))
+					if(search==None):
+						pprint("Error:Studio not found")
+						continue;
+					g.add((SerieNode,ns.hasStudio,search))
+			if(attr=='Start'):
+				g.add((SerieNode,ns.hasSerieStart,Literal(serie[attr], datatype=XSD.integer)))
+			if(attr=='End'):
+				g.add((SerieNode,ns.hasSerieEnd,Literal(serie[attr], datatype=XSD.integer)))
+			if(attr=='genres'):
+				for genre in serie[attr]['genre']:
+					g.add((SerieNode,ns.hasGenre,ns[genre]))
+			if(attr=='description'):
+				g.add((SerieNode,ns.hasMediaDescription,Literal(serie[attr])))
+			if(attr=='score'):
+				g.add((SerieNode,ns.hasMediaClassification,Literal(serie[attr], datatype=XSD.float)))
+			if(attr=='duration'):
+				g.add((SerieNode,ns.hasMediaDuration,Literal(serie[attr], datatype=XSD.integer)))
+			if(attr=='stars'):
+				for star in serie[attr]['star']:
+					search = g.value(predicate=ns.hasPersonId,object=Literal(star))
+					if(search==None):
+						pprint("Error:Star not found")
+						continue;
+					g.add((SerieNode,ns.hasActor,search))
+			if(attr=='name'):
+				g.add((SerieNode,ns.hasMediaName,Literal(serie[attr])))
+			if(attr=='image'):
+				g.add((SerieNode,ns.hasMediaPoster,Literal(serie[attr])))
+			if(attr=='seasons'):
+				g.add((SerieNode,ns.hasSerieSeason,Literal(serie[attr], datatype=XSD.integer)))
+
+
 
 	thisYear += 1
 
@@ -177,7 +236,7 @@ while thisYear <= lastYear:
 			continue;
 		try:
 			for relevant in person['knownFor']:
-				search = g.value(predicate=ns.hasMovieId, object=Literal(relevant))
+				search = g.value(predicate=ns.hasMediaId, object=Literal(relevant))
 				if search == None:
 					pprint("Error: this better be a series: " + relevant)
 					continue
