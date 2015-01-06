15c87a6013fde54f7c72745a261f116839c49538
jfms7s <jfms7s@gmail.com>
2015-01-06 22:44:23 +0000

    Added Class type to movie

--- IMDB_Server/src/Service/MoviesManager.java
+++ IMDB_Server/src/Service/MoviesManager.java
@@ -1,43 +1,45 @@
 package Service;
 
-import com.google.gson.Gson;
-import com.hp.hpl.jena.query.*;
-import com.hp.hpl.jena.rdf.model.Model;
-import com.hp.hpl.jena.rdf.model.ModelFactory;
-import com.hp.hpl.jena.tdb.TDBFactory;
-import com.hp.hpl.jena.util.FileManager;
 import org.json.JSONArray;
-
-import java.io.ByteArrayOutputStream;
-import java.io.File;
 
 public class MoviesManager extends Connection{
     private String GlobalNamespace="<http://www.movierecomendation.pt/ontology/movierecomendation.owl#>";
+    private String RdfNamespace="<http://www.w3.org/1999/02/22-rdf-syntax-ns#>";
+    private String RdfsNamespace="<http://www.w3.org/2000/01/rdf-schema#>";
+
+    String StandardSelect = " ?id ?Name ?typeUri ?Poster ?duration ?launched ?description ?classification ";
+
+    String StandardWhere = "?MovieUri ns:hasMediaName ?Name; ns:hasMediaId ?id; rdf:type ?typeUri; ns:hasMediaPoster ?Poster; ns:hasMediaDuration ?duration; ns:hasMediaDescription ?description; ns:hasMediaClassification ?classification . ?typeUri rdfs:subClassOf ns:Media. OPTIONAL { ?b ns:hasMediaLaunchDate ?launched } . OPTIONAL { ?b ns:hasSerieEnd ?end } . OPTIONAL { ?b ns:hasSerieStart ?start } . OPTIONAL { ?b ns:hasSerieSeasons ?seasons } .";
+
 
     String queryGetAll =
                     "PREFIX ns: "+GlobalNamespace+" "+
-                    "SELECT ?id ?Name ?Poster ?duration ?launched ?description ?classification " +
-                    "WHERE {?b ns:hasMediaName ?Name; ns:hasMediaId ?id; ns:hasMediaPoster ?Poster; ns:hasMediaDuration ?duration; ns:hasMediaDescription ?description;" +
-                            " ns:hasMediaClassification ?classification . OPTIONAL { ?b ns:hasMediaLaunchDate ?launched } . OPTIONAL { ?b ns:hasSerieEnd ?end } . OPTIONAL { ?b ns:hasSerieStart ?start } . OPTIONAL { ?b ns:hasSerieSeasons ?seasons } . }";
+                    "PREFIX rdf: "+RdfNamespace+" "+
+                    "PREFIX rdfs: "+RdfsNamespace+" "+
+                    "SELECT "+ StandardSelect +
+                    "WHERE { "+ StandardWhere +" }";
     String queryGetById =
-            "PREFIX ns: "+GlobalNamespace+" "+
-                    "SELECT ?id ?Name ?Poster ?duration ?launched ?description ?classification " +
-                    "WHERE {?b ns:hasMediaName ?Name; ns:hasMediaId ?id; ns:hasMediaPoster ?Poster; ns:hasMediaDuration ?duration; ns:hasMediaDescription ?description;" +
-                    " ns:hasMediaClassification ?classification . OPTIONAL { ?b ns:hasMediaLaunchDate ?launched } . OPTIONAL { ?b ns:hasSerieEnd ?end } . OPTIONAL { ?b ns:hasSerieStart ?start } . OPTIONAL { ?b ns:hasSerieSeasons ?seasons } ." +
-                    "FILTER regex(?id, \"%s\") }";
+                    "PREFIX ns: "+GlobalNamespace+" "+
+                    "PREFIX rdf: "+RdfNamespace+" "+
+                    "PREFIX rdfs: "+RdfsNamespace+" "+
+                    "SELECT "+ StandardSelect +
+                    "WHERE { " + StandardWhere + " FILTER regex(?id, \"%s\") }";
     String queryGetMostRecent =
-            "PREFIX ns: "+GlobalNamespace+" "+
-                    "SELECT ?id ?Name ?Poster ?duration ?launched ?description ?classification " +
-                    "WHERE { ?b ns:hasMediaName ?Name; ns:hasMediaId ?id; ns:hasMediaPoster ?Poster; " +
-                    "ns:hasMediaDuration ?duration; ns:hasMediaLaunchDate ?launched; ns:hasMediaDescription ?description; ns:hasMediaClassification ?classification .} "+
+                    "PREFIX ns: "+GlobalNamespace+" "+
+                    "PREFIX rdf: "+RdfNamespace+" "+
+                    "PREFIX rdfs: "+RdfsNamespace+" "+
+                    "SELECT "+ StandardSelect +
+                    "WHERE { "+ StandardWhere +" } "+
                     "ORDER BY ?launched "+
                     "LIMIT 6";
     String querySelectGenre=
-            "PREFIX ns: "+GlobalNamespace+" "+
-                    "SELECT DISTINCT ?id ?Name ?Poster ?duration ?launched ?description ?classification " +
+                    "PREFIX ns: "+GlobalNamespace+" "+
+                    "PREFIX rdf: "+RdfNamespace+" "+
+                    "PREFIX rdfs: "+RdfsNamespace+" "+
+                    "SELECT DISTINCT "+ StandardSelect +
                     "WHERE {%s} ";
     String queryWhereGenre=
-            "{ ?b ns:hasGenres ns:%s ; ns:hasMovieName ?Name; ns:hasMovieId ?id; ns:hasMoviePoster ?Poster; ns:hasMovieDuration ?duration; ns:hasMovieLaunchDate ?launched; ns:hasMovieDescription ?description; ns:hasMovieClassification ?classification .}"
+            "{ ?MovieUri ns:hasGenres ns:%s . "+ StandardWhere +" }"
 
             ;
 
