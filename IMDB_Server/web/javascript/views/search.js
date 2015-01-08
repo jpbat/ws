ae9b07e91745de9bcd769a91fc31bc3730439786
jfms7s <jfms7s@gmail.com>
2015-01-08 23:13:41 +0000

    fixes to search

--- IMDB_Server/web/javascript/views/search.js
+++ IMDB_Server/web/javascript/views/search.js
@@ -4,6 +4,9 @@ define(
         return Backbone.View.extend({
             tagName: "div",
             id: "SearchResult",
+            minChars: 3,
+            limit:20,
+            offset:0,
             //className: "col-md-2",
             tpl: template,
             initialize: function () {
@@ -12,12 +15,62 @@ define(
             render: function () {
                 var Result = this.collection.toJSON();
 
+                if(Result.length ==0){
+                    return this;
+                }
+
                 var templateHTML = this.tpl({collection: Result});
-                this.$el.html(templateHTML);
+                this.$el.append(templateHTML);
 
                 return this;
             },
-            events: {}
+            updateSearch:function(data){
+                var self = this;
+                self.offset=0;
+                this.$el.html(" ");
+                if(data.length>=this.minChars){
+                    this.data = data;
+                    this.collection.fetch({
+                        data: {limit:self.limit,offset:self.offset,query: data},
+                        reset: true, type: 'GET',
+                        success: function () {
+                            self.offset+=self.limit;
+                            console.log("Got it");
+                        },
+                        error: function () {
+                            console.log("error");
+                        }
+                    });
+                }else{
+                    this.data="";
+                }
+
+            },
+            events: {'scroll': 'checkScroll'},
+            checkScroll: function () {
+                var self = this;
+                var triggerPoint = 100; // 100px from the bottom
+                if( !this.isLoading && this.el.scrollTop + this.el.clientHeight + triggerPoint > this.el.scrollHeight ) {
+                    this.isLoading = true;
+
+                    self.collection.fetch({data:{limit:self.limit,offset:self.offset,query: self.data},
+                        type: 'GET',
+                        success: function () {
+
+                            self.offset+=self.limit;
+                            self.render();
+                            self.isLoading = false;
+                        },
+                        error: function () {
+                            var newElement = $('#alertContainer div').clone();
+                            $(newElement).find("p").html("Fail to retrieve query Results");
+                            $('.alertContainer').append(newElement);
+                            console.log("Fail to retrieve query Results");
+                        }
+                    });
+
+                }
+            }
         });
     }
 );
