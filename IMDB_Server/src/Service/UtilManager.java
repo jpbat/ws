f9121c435d1ba9badfd80b76198e0ee775fb688b
jfms7s <jfms7s@gmail.com>
2015-01-10 02:19:48 +0000

    fix

--- IMDB_Server/src/Service/UtilManager.java
+++ IMDB_Server/src/Service/UtilManager.java
@@ -5,13 +5,23 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 
 import javax.ws.rs.QueryParam;
+import java.util.ArrayList;
 
-/**
- * Created by Joao on 07/01/2015.
- */
+
 public class UtilManager extends Connection {
 
-    String StringMatchingQuery = StrUtils.strjoinNL
+    static ArrayList<String> ProfessionList;
+    static ArrayList<String> GenreList;
+
+    private enum Type{
+        NONE,
+        GENRE,
+        PROFESSION,
+        NAME,
+        MOVIE,
+        SERIE}
+
+    private String StringMatchingQuery = StrUtils.strjoinNL
                      ( "PREFIX ns: <http://www.movierecomendation.pt/ontology/movierecomendation.owl#>"
                      , "PREFIX text: <http://jena.apache.org/text#>"
                      , "PREFIX owl: <http://www.w3.org/2002/07/owl#>"
@@ -38,11 +48,62 @@ public class UtilManager extends Connection {
     }
 
     public JSONArray StringMatching(String text){
+
         return AddInfo(this.PerformQuery(String.format(StringMatchingQuery, text)));
     }
 
     public JSONArray StringMatching(String text, int offset, int limit){
 
+        if(GenreList==null || ProfessionList ==null){
+            ProfessionManager pService = new ProfessionManager();
+            GenresManager gService = new GenresManager();
+
+            ProfessionList = new ArrayList<String>();
+            GenreList = new ArrayList<String>();
+
+            JSONArray GenreJson = gService.GetAll();
+            JSONArray ProfessionJson = pService.GetAll();
+
+            for (int i = 0; i <GenreJson.length(); i++) {
+                JSONObject aux = (JSONObject)GenreJson.get(i);
+                GenreList.add(aux.getString("name").toLowerCase());
+            }
+
+            for (int i = 0; i <ProfessionJson.length(); i++) {
+                JSONObject aux = (JSONObject)ProfessionJson.get(i);
+                ProfessionList.add(aux.getString("name").toLowerCase());
+            }
+        }
+
+        String [] queryElements = text.toLowerCase().split("\\s+");
+
+        Type [] ElementTypes = new Type[queryElements.length];
+
+        for (int i = 0; i < queryElements.length; i++) {
+            if(GenreList.contains(queryElements[i])){
+                ElementTypes[i]=Type.GENRE;
+                System.out.println("Genre");
+                continue;
+            }
+            if(ProfessionList.contains(queryElements[i])){
+                System.out.println("Profession");
+                ElementTypes[i]=Type.PROFESSION;
+                continue;
+            }
+            if("serie"==queryElements[i]){
+                System.out.println("Serie");
+                ElementTypes[i]=Type.SERIE;
+                continue;
+            }
+            if("movie"==queryElements[i]){
+                System.out.println("movie");
+                ElementTypes[i]=Type.MOVIE;
+                continue;
+            }
+
+            ElementTypes[i]=Type.NONE;
+            System.out.println("NONE");
+        }
         return AddInfo(this.PerformQuery(String.format(StringMatchingQuery, text)+" LIMIT "+limit+" OFFSET "+offset));
     }
 
